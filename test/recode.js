3b51df4593d2c8cb54a7a56d4d3fbc0d3f75b1eb
James Halliday <mail@substack.net>
2015-11-02 01:33:03 -0800

    fixed trailing zeros

--- test/recode.js
+++ test/recode.js
@@ -3,7 +3,7 @@ var test = require('tape')
 
 test('recode', function (t) {
   t.plan(1)
-  var bits = [1,1,1,0,1,1,1,0,0,1,0,0,0,0,1,1,1,1,1,0,0,0,0,1,1,0]
+  var bits = [1,1,1,0,1,1,1,0,0,1,0,0,0,0,1,1,1,1,1,0,0,0,0,1,1]
   var tree = succinct.decode(bits)
   t.deepEqual(succinct.encode(tree), bits, 're-encoded')
 })
